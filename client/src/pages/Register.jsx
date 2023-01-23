9c2341f24cc65e35f695efa23e89293671285438
Prajna <prajnapprabhu3@gmail.com>
2023-01-23 23:44:30 +0530

    dependency client

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,30 @@
+
+
+function Register() {
+  return (
+    <div className='h-screen flex justify-center items-center bg-[#17181D] text-white'>
+     
+     <div className='formContainer bg-[#21222B] rounded-2xl'>
+      
+      {/* register content  */}
+      <div className='flex flex-col items-center'>
+      <p className='text-xl font-semibold'>User Register</p>
+      <div className='flex flex-col items-center text-xs font-light text-zinc-400'>
+        <p>Welcome, Enter your details to sign in</p>
+        <p>to your account</p>
+      </div>
+      </div>
+
+
+      {/* form section  */}
+      <form action="">
+        
+      </form>
+
+
+     </div>
+    </div>
+  )
+}
+
+export default Register
--- server/index.js
+++ server/index.js
@@ -4,3 +4,19 @@ const cors=require('cors');
 require('dotenv').config();
 
 const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log('Database connected successfully')
+}).catch((err)=>{
+    console.log(err.message);
+});
+
+const server=app.listen(process.env.PORT,()=>{
+    console.log(`server running on http://localhost:${process.env.PORT}`);
+})
